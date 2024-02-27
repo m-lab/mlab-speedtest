55d3f66be7fc7c1d243dac6f69a688e716cc08fe
Roberto D'Auria <roberto@measurementlab.net>
2024-02-27 13:26:46 +0100

    Add docstring on function uuidv4

--- app/measure/measure.js
+++ app/measure/measure.js
@@ -184,7 +184,15 @@ angular.module('Measure.Measure', ['ngRoute'])
     }
   });
 
+/**
+ * Generates a UUIDv4.
+ *
+ * See: https://en.wikipedia.org/wiki/Universally_unique_identifier#Version_4_(random)
+ *
+ * @returns
+ */
 function uuidv4() {
+  // From https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid
   return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
     (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
   );
